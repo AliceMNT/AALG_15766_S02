4d0a4799bf3b912f46173bade3426d2f8e628122
AlicieMNT <aliciam.nunez.t@gmail.com>
2025-04-04 19:43:30 -0500

    correción doble linea de ingreso

---
+++ Ingreso_datos.py
@@ -0,0 +1,8 @@
+print("¿Comó te llamas?")
+n = input()
+print(f"hola {n}")
+
+
+n = input ("Como te llamas\n")
+print (f"hola {n}")
+
---
+++ program.py
@@ -0,0 +1,6 @@
+a = 2**3
+
+if a == 8: 
+    print(a)
+else:
+    print("No")
